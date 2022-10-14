f057d5f0a4d74cb41278c66db7943409ee0072cd
Léa M <mhoumadi.lea@gmail.com>
2022-10-14 15:20:44 +0200

    refactor: insert data into posts table

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,11 +1,43 @@
 <?php
 session_start();
+require('../includes/pdo.php');
+
+
+$submit = filter_input(INPUT_POST, 'submit');
+$post = filter_input(INPUT_POST, 'post');
+$user_id = $_SESSION['id'];
+
+
+if (isset($submit)) {
+    if (!empty($post)) {
+        $insertPostData = $pdo->prepare("INSERT INTO posts (content, user_id) VALUES (:post, :user_id)");
+        $insertPostData->execute([
+            ":post" => $post,
+            ":user_id" => $_SESSION['id'],
+        ]);
+    }
+}
+
+$postsData = $pdo->query("SELECT * FROM posts");
+$allPosts = $postsData->fetchAll(PDO::FETCH_ASSOC)
+?>
+
+<?php
 require('../partials/header.php');
 ?>
 
 
 <a href="../includes/logout.inc.php">Log out</a>
-<h1>Welcome on your profile <?= $_SESSION['username'] ?></h1>
+
+<div class="homeWrapper">
+    <h1 class="homeTitle">Welcome on your profile <?= $_SESSION['username'] ?></h1>
+    <form class="formPost" method="POST">
+        <input class="postInput" type="text" name="post" placeholder="Write your post...">
+        <input type="submit" value="Publish" name="submit">
+    </form>
+</div>
+
+
 
 
 <?php
